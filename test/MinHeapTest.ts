0b236a42ab12cef0b85802bffcfddf0b57499c3b
Florin Buga <>
2020-01-11 22:12:50 +0200

    docs: rewrite tests' descriptions

--- test/MinHeapTest.ts
+++ test/MinHeapTest.ts
@@ -2,38 +2,42 @@ import { assert } from "chai";
 import MinHeap from "../src/MinHeap";
 
 describe("MinHeap", () => {
-  it("Should insert", () => {
-    const minHeap = new MinHeap();
-    minHeap.insert(3);
-    minHeap.insert(5);
-    minHeap.insert(1);
-    minHeap.insert(4);
-    minHeap.insert(0);
-    assert.deepEqual(minHeap.getValues(), [0, 1, 3, 5, 4]);
+  describe("insert()", () => {
+    it("Should insert and bubble up small values", () => {
+      const minHeap = new MinHeap();
+      minHeap.insert(3);
+      minHeap.insert(5);
+      minHeap.insert(1);
+      minHeap.insert(4);
+      minHeap.insert(0);
+      assert.deepEqual(minHeap.getValues(), [0, 1, 3, 5, 4]);
+    });
   });
 
-  it("Should extract", () => {
-    const minHeap = new MinHeap();
-    minHeap.insert(3);
-    minHeap.insert(5);
-    minHeap.insert(1);
-    minHeap.insert(4);
-    minHeap.insert(0);
-    minHeap.insert(9);
-    assert.equal(minHeap.extractMin(), 0);
-    assert.deepEqual(minHeap.getValues(), [1, 4, 3, 5, 9]);
-  });
+  describe("extractMin()", () => {
+    it("Should work with a 1 node tree", () => {
+      const minHeap = new MinHeap();
+      minHeap.insert(3);
+      assert.equal(minHeap.extractMin(), 3);
+      assert.deepEqual(minHeap.getValues(), []);
+    });
 
-  it("Should extract in a 1 node tree", () => {
-    const minHeap = new MinHeap();
-    minHeap.insert(3);
-    assert.equal(minHeap.extractMin(), 3);
-    assert.deepEqual(minHeap.getValues(), []);
-  });
+    it("Should extract null in an empty tree", () => {
+      const minHeap = new MinHeap();
+      assert.equal(minHeap.extractMin(), null);
+      assert.deepEqual(minHeap.getValues(), []);
+    });
 
-  it("Should extract null in an empty tree", () => {
-    const minHeap = new MinHeap();
-    assert.equal(minHeap.extractMin(), null);
-    assert.deepEqual(minHeap.getValues(), []);
+    it("Should extract and bubble down big values", () => {
+      const minHeap = new MinHeap();
+      minHeap.insert(3);
+      minHeap.insert(5);
+      minHeap.insert(1);
+      minHeap.insert(4);
+      minHeap.insert(0);
+      minHeap.insert(9);
+      assert.equal(minHeap.extractMin(), 0);
+      assert.deepEqual(minHeap.getValues(), [1, 4, 3, 5, 9]);
+    });
   });
 });
