3c038bdddc3428c5a1481a4529916464e13778e9
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-12 14:44:00 +0100

    Update MinHeap.ts

--- src/MinHeap.ts
+++ src/MinHeap.ts
@@ -4,16 +4,9 @@ import CompleteBinaryTreeInArray from "./CompleteBinaryTreeInArray";
  * A Min-Heap is a Complete Binary Tree in which any parent node value is less than its children's.
  */
 export default class MinHeap extends CompleteBinaryTreeInArray<number> {
+  
   /**
-   * Insert value
-   *
    * Insert as Last Node such that to maintain a Complete Binary Tree
-   * (see the explanation on the Last Node in the Complete Binary Tree interface description
-   * https://github.com/zendka/algorithms-and-data-structures/blob/master/src/CompleteBinaryTree.ts)
-   *
-   * Then "bubble up" the node: move it higher in the tree until the Min-Heap is restored
-   * i.e. parent's value is less than its childrens'.
-   *
    * O(log n) time.
    */
   insert(value: number): void {
@@ -26,18 +19,9 @@ export default class MinHeap extends CompleteBinaryTreeInArray<number> {
   }
 
   /**
-   * Extract min
-   *
    * The min value is in the root node.
-   * It's not possible to just remove the root in a Complete Binary Tree quality.
-   * Only the Last Node can be removed, so swipe it with root, then remove it.
-   * (see the explanation on the Last Node in the Complete Binary Tree interface description
-   * https://github.com/zendka/algorithms-and-data-structures/blob/master/src/CompleteBinaryTree.ts)
-   *
-   * The new root is not necessarily the smallest value.
-   * We need to "sink down" the new root: move it down until the Min-Heap is restored
-   * i.e. parent's value is smaller than its children's.
-   *
+   * We can't remove the root in a Complete Binary Tree. 
+   * Only the Last Node can be removed, so swap it with root, then remove it.
    * O(log n) time
    */
   extractMin(): number {
