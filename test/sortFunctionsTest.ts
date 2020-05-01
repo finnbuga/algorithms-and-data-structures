b0918f420b689826baa07dee1c5e27f6ba4e674b
Florin <florin.buga@gmail.com>
2020-05-01 13:38:07 +0300

    refactor: reduce the number of arguments for mergeSort()

--- test/sortFunctionsTest.ts
+++ test/sortFunctionsTest.ts
@@ -1,28 +1,24 @@
 import { assert } from "chai";
-import mergeSort from "../src/mergeSort";
+import sortSlice from "../src/mergeSort";
 import quickSort from "../src/quickSort";
 
-[mergeSort, quickSort].forEach((sort) => {
+[sortSlice, quickSort].forEach((sort) => {
   describe(sort.name, () => {
     it("Should sort a random array", () => {
       const a = [4, 6, 2, 8, 9, 5];
-      sort(a);
-      assert.deepEqual(a, [2, 4, 5, 6, 8, 9]);
+      assert.deepEqual(sort(a), [2, 4, 5, 6, 8, 9]);
     });
     it("Should work on en empty array", () => {
       const a = [];
-      sort(a);
-      assert.deepEqual(a, []);
+      assert.deepEqual(sort(a), []);
     });
     it("Should sort a reversly sorted array", () => {
       const a = [6, 4, 3, 2, 1, 0];
-      sort(a);
-      assert.deepEqual(a, [0, 1, 2, 3, 4, 6]);
+      assert.deepEqual(sort(a), [0, 1, 2, 3, 4, 6]);
     });
     it("Should sort negative numbers", () => {
       const a = [-6, 4, 3, 2, 1, 0];
-      sort(a);
-      assert.deepEqual(a, [-6, 0, 1, 2, 3, 4]);
+      assert.deepEqual(sort(a), [-6, 0, 1, 2, 3, 4]);
     });
   });
 });
