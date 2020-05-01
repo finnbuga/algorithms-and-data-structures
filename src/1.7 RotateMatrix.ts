8ecbd9065ac0a6a59cf49874ecf9e60b418a7627
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-05-01 09:16:24 +0300

    refactor: reduce the number of arguments

--- src/1.7 RotateMatrix.ts
+++ src/1.7 RotateMatrix.ts
@@ -1,29 +1,35 @@
-type Matrix = Array<Array<any>>;
+export type Matrix = Array<Array<any>>;
 
-export default function rotateMatrix(m: Matrix): void {
-  for (let row: number = 0; row <= m.length / 2; row++) {
-    rotateRow(m, row);
+export default class RotatingMatrix {
+  constructor(private matrix: Matrix) {}
+
+  rotate(): Matrix {
+    for (let row = 0; row <= this.matrix.length / 2; row++) {
+      this.rotateRow(row);
+    }
+    return this.matrix;
   }
-}
 
-function rotateRow(m: Matrix, row: number): void {
-  for (let col: number = row; col < m.length - 1; col++) {
-    rotateElm(m, row, col);
+  private rotateRow(row: number): void {
+    for (let col: number = row; col < this.matrix.length - 1; col++) {
+      this.rotateElm(row, col);
+    }
   }
-}
 
-function rotateElm(m: Matrix, row: number, col: number): void {
-  const row1: number = col,
-    col1: number = m.length - 1 - row,
-    row2: number = col1,
-    col2: number = m.length - 1 - row1,
-    row3: number = col2,
-    col3: number = m.length - 1 - row2;
+  private rotateElm(row: number, col: number): void {
+    const m = this.matrix;
+    const row1 = col,
+      col1 = m.length - 1 - row,
+      row2 = col1,
+      col2 = m.length - 1 - row1,
+      row3 = col2,
+      col3 = m.length - 1 - row2;
 
-  [m[row][col], m[row1][col1], m[row2][col2], m[row3][col3]] = [
-    m[row3][col3],
-    m[row][col],
-    m[row1][col1],
-    m[row2][col2]
-  ];
+    [m[row][col], m[row1][col1], m[row2][col2], m[row3][col3]] = [
+      m[row3][col3],
+      m[row][col],
+      m[row1][col1],
+      m[row2][col2],
+    ];
+  }
 }
