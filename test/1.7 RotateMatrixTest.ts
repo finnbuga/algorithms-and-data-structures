98e730a116b0c6431fb8ed14bf86e47d0932e79d
Florin <florin.buga@gmail.com>
2020-05-01 09:16:24 +0300

    refactor: reduce the number of arguments

--- test/1.7 RotateMatrixTest.ts
+++ test/1.7 RotateMatrixTest.ts
@@ -1,50 +1,46 @@
 import { assert } from "chai";
-import rotateMatrix from "../src/1.7 RotateMatrix";
+import RotatingMatrix, { Matrix } from "../src/1.7 RotateMatrix";
 
 describe("rotateMatrix", () => {
   it("should rotate a 0x0 matrix", () => {
-    const m: Array<Array<any>> = [];
-    const mRotated: Array<Array<any>> = [];
+    const m = new RotatingMatrix([]);
+    const mRotated: Matrix = [];
 
-    rotateMatrix(m);
-    assert.deepEqual(m, mRotated);
+    assert.deepEqual(m.rotate(), mRotated);
   });
 
   it("should rotate a 1x1 matrix", () => {
-    const m: Array<Array<any>> = [[1]];
-    const mRotated: Array<Array<any>> = [[1]];
+    const m = new RotatingMatrix([[1]]);
+    const mRotated: Matrix = [[1]];
 
-    rotateMatrix(m);
-    assert.deepEqual(m, mRotated);
+    assert.deepEqual(m.rotate(), mRotated);
   });
 
   it("should rotate a 2x2 matrix", () => {
-    const m: Array<Array<any>> = [
+    const m = new RotatingMatrix([
       [1, 2],
-      [3, 4]
-    ];
-    const mRotated: Array<Array<any>> = [
+      [3, 4],
+    ]);
+    const mRotated: Matrix = [
       [3, 1],
-      [4, 2]
+      [4, 2],
     ];
 
-    rotateMatrix(m);
-    assert.deepEqual(m, mRotated);
+    assert.deepEqual(m.rotate(), mRotated);
   });
 
   it("should rotate a 3x3 matrix", () => {
-    const m: Array<Array<any>> = [
+    const m = new RotatingMatrix([
       [1, 2, 3],
       [4, 5, 6],
-      [7, 8, 9]
-    ];
-    const mRotated: Array<Array<any>> = [
+      [7, 8, 9],
+    ]);
+    const mRotated: Matrix = [
       [7, 4, 1],
       [8, 5, 2],
-      [9, 6, 3]
+      [9, 6, 3],
     ];
 
-    rotateMatrix(m);
-    assert.deepEqual(m, mRotated);
+    assert.deepEqual(m.rotate(), mRotated);
   });
 });
