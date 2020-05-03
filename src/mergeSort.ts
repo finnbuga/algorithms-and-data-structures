041fd5ffc4c546761a0384fba963ef702149d85d
Florin <florin.buga@gmail.com>
2020-05-03 09:01:36 +0300

    refactor: improve readability of mergeSort()

--- src/mergeSort.ts
+++ src/mergeSort.ts
@@ -11,26 +11,27 @@ export default function sortSlice(a: number[]) {
     const mid = Math.floor((begin + end) / 2);
     sortSlice(begin, mid);
     sortSlice(mid, end);
-    mergeSortedSlices();
+    mergeSortedSlices(begin, mid, end);
+  }
 
-    function mergeSortedSlices(): void {
-      const slice1Begin = begin,
-        slice1End = mid,
-        slice2Begin = mid,
-        slice2End = end;
-      const sorted: number[] = [];
-      let i1: number = slice1Begin;
-      let i2: number = slice2Begin;
+  function mergeSortedSlices(
+    s1Begin: number,
+    s1End: number,
+    s2End: number
+  ): void {
+    const s2Begin = s1End;
+    const sorted: number[] = [];
+    let i1: number = s1Begin;
+    let i2: number = s2Begin;
 
-      while (i1 < slice1End && i2 < slice2End) {
-        sorted.push(a[i1] <= a[i2] ? a[i1++] : a[i2++]);
-      }
-
-      while (i1 < slice1End) {
-        sorted.push(a[i1++]);
-      }
-
-      a.splice(begin, sorted.length, ...sorted);
+    while (i1 < s1End && i2 < s2End) {
+      sorted.push(a[i1] <= a[i2] ? a[i1++] : a[i2++]);
     }
+
+    while (i1 < s1End) {
+      sorted.push(a[i1++]);
+    }
+
+    a.splice(s1Begin, sorted.length, ...sorted);
   }
 }
